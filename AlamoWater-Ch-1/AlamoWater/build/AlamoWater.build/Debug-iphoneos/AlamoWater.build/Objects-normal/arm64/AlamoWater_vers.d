7e0f5bd8f1395c3347ac092b1cb3e3f1721ab786
RAJASEKHAR <rspadala88@gmail.com>
2018-05-01 21:36:12 +0530

    custome framework added

---
+++ AlamoWater-Ch-1/AlamoWater/build/AlamoWater.build/Debug-iphoneos/AlamoWater.build/Objects-normal/arm64/AlamoWater_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/iosteam/Downloads/AlamoWater-Ch-1/AlamoWater/build/AlamoWater.build/Debug-iphoneos/AlamoWater.build/DerivedSources/AlamoWater_vers.c
---
+++ AlamoWater-Ch-1/AlamoWater/build/AlamoWater.build/Debug-iphonesimulator/AlamoWater.build/Objects-normal/i386/AlamoWater_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/iosteam/Downloads/AlamoWater-Ch-1/AlamoWater/build/AlamoWater.build/Debug-iphonesimulator/AlamoWater.build/DerivedSources/AlamoWater_vers.c
